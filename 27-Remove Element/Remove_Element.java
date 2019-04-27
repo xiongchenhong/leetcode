a56dabc93fc3f5cd2d15a4ca0b2548ef091563d6
xiongchenhong <xchmail0@gmail.com>
2019-04-27 23:39:22 +0800

    Remove Element

---
+++ 27-Remove Element/Remove_Element.java
@@ -0,0 +1,31 @@
+class Solution {
+    // 时间复杂度O(n)
+    // 空间复杂度O(1)
+    public int removeElement1(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    // 时间复杂度O(n)
+    // 空间复杂度O(1)
+    // 被移除的元素很少时效率更高
+    public int removeElement2(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return i;
+    }
+}
